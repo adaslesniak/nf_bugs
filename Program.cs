b62fb85ce198e4e0758d07a86e2710e81d9ce25d
adas <adam.lesniak@gc5.pl>
2020-07-07 11:08:02 +0200

    refactored for clarity and tested with newest extension

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Reflection;
 using System.Threading;
 using SourceLibrary;
 
@@ -5,9 +7,14 @@ using SourceLibrary;
 namespace TestProject
 {
 
-    class Implementation : BaseClass, IInterface { }
+    class Implementation : BaseClass, IInterface {
+        public void anyMethod(int withIntParam) { }
+    }
+
 
     class ImplmentationByInheritance : Implementation { }
+    
+    enum AnyEnum {  One, Two, Three }
       
 
     public class Program
@@ -15,22 +22,50 @@ namespace TestProject
 
         public static void Main()
         {
+            var testObject = new ImplmentationByInheritance();
+            try {
+                TestOne(testObject);
+                Console.WriteLine("Test 1 passed");
+            } catch(Exception error) {
+                Console.WriteLine("Test 1 failed: " + error.Message);
+            }
+            try {
+                TestTwo(testObject);
+                Console.WriteLine("Test 2 passed");
+            } catch(Exception error) {
+                Console.WriteLine("Test 2 failed: " + error.Message);
+            }
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        static void TestOne(ImplmentationByInheritance testObject)
+        {
 
             //this bug is not replicable if base class and interface are defined in executed project
             //it happens when they are defined in separate library
 
-            var asImplementationByInheritance = new ImplmentationByInheritance();
-            Print(asImplementationByInheritance is IInterface, "ImplmentationByInheritance");
-            var asBaseClass = asImplementationByInheritance as BaseClass;
-            Print(asBaseClass is IInterface, "BaseClass");
-            
-
-            Thread.Sleep(Timeout.Infinite);
-
-            void Print(bool isTrue, string objectType)
+            var isFirstCase = testObject is IInterface;
+            var asBaseClass = testObject as BaseClass;
+            var isSecondCase = asBaseClass is IInterface;
+            if(isFirstCase && isSecondCase == false)
             {
-                var header = $"[{isTrue}]";
-                System.Console.WriteLine($"{header} the same object as {objectType} is IInterface");
+                throw new System.Exception($"[{isFirstCase}] & [{isSecondCase}] Something is wrong " + 
+                    "checking for interface implementation works differently " +
+                    "depending how object is referenced. ");
+                //looks like "is" is checking for target.GetTypeOfReferenceNotOfReferenced() but only for Interfaces
+            }
+        }
+
+        static void TestTwo(ImplmentationByInheritance testObject)
+        {
+            var isFirstCase = testObject is ImplmentationByInheritance;
+            var asBaseClass = testObject as BaseClass;
+            var isSecondCase = asBaseClass is ImplmentationByInheritance;
+            if(isFirstCase && isSecondCase == false)
+            {
+                throw new System.Exception("[{isFirstCase}] & [{isSecondCase}] Something is wrong " +
+                    "checking for interface implementation works differently " +
+                    "depending how object is referenced. ");
             }
         }
     }
